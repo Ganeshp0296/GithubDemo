cbd482bbcbc30ef900508b60e2f35fd6fd5f6d50
GaneshP0296 <ganeshp457@gmail.com>
2023-06-21 08:52:17 +0530

    Added:New Method

--- src/com/bl/practice/Demo.java
+++ src/com/bl/practice/Demo.java
@@ -1,7 +1,15 @@
 package com.bl.practice;
 
 public class Demo {
+
+    void displayMessage(){
+        System.out.println("Hello");
+    }
+
     public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.displayMessage();
         System.out.println("Ganesh");
     }
+
 }
